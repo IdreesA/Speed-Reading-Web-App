51920757c590f0c03fb64e9a569b16339cdd193a
IdreesA <idrees.philly@gmail.com>
2020-12-04 23:56:34 -0500

    added gitignore

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        text = request.form["Text"]
+        print(text)
+        return redirect(url_for(wbw, text=text))
     return render_template("index.html")
+
+
+
+@app.route("/wordbyword/", methods=["GET", "POST"])
+def wbw():
+    return "Hi"
+
+
+
+
+
 
 
 if __name__ == "__main__":
